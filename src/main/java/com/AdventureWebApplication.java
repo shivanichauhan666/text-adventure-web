a5dc136a21c0a7f70b5a1bd3f09fe3b77dfdae9e
Shivani Chauhan <shivanichauhan3455@gmail.com>
2026-04-08 21:41:30 +0530

    Initial commit - text adventure game

---
+++ src/main/java/com/AdventureWebApplication.java
@@ -0,0 +1,11 @@
+package com.example.adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureWebApplication.class, args);
+    }
+}
